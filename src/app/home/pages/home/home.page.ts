9af9672e1dfc8b3b64b0ded5d90ac8ced5722d10
Andy Williamson <email@andywilliamson.io>
2020-07-20 01:54:18 +0100

    Added changes, to simplify loading and clearing of data

--- src/app/home/pages/home/home.page.ts
+++ src/app/home/pages/home/home.page.ts
@@ -1,6 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {HomeHelperService} from '../../services/helper.service';
-import {Observable} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
+import * as fromDataReducers from '../../store/data/reducers';
+import {select, Store} from '@ngrx/store';
+import {HomePageState} from '../../store/data/reducers';
+import {Region} from '../../models/region.model';
 
 @Component({
     selector: 'app-base-home',
@@ -8,19 +12,27 @@ import {Observable} from 'rxjs';
     styleUrls: ['./home.page.scss']
 })
 
-export class HomePageComponent {
-    public regions$: Observable<any>;
+export class HomePageComponent implements OnInit, OnDestroy{
     public regionsNGRX$: Observable<any>;
+    public regionsNGRX: Region[] = [];
+    public regionsNGRXSubscription: Subscription;
 
     constructor(
-        public homeHelperService: HomeHelperService
-    ) {}
-
-    fetch() {
-        this.regions$ = this.homeHelperService.fetchUsingHttp();
+        public homeHelperService: HomeHelperService,
+        private store: Store<HomePageState>,
+    ) {
+        this.regionsNGRX$ = this.store.pipe(
+            select(fromDataReducers.getAllRegions)
+        );
     }
 
-    fetchNGRX() {
-        this.regionsNGRX$ = this.homeHelperService.fetchUsingHttpAndNGRX();
+    ngOnInit() {
+        this.regionsNGRXSubscription = this.regionsNGRX$.subscribe((data: Region[]) => {
+            this.regionsNGRX = data;
+        })
+    }
+
+    ngOnDestroy() {
+        this.regionsNGRXSubscription.unsubscribe();
     }
 }
